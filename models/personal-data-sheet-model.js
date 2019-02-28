88b01cc89dc2aaf56670ce81a864b50ac9ef7be7
Jesse Dino <jesse.dino.z3@gmail.com>
2019-02-28 14:10:24 +0800

    updated PDSmodel

--- models/personal-data-sheet-model.js
+++ models/personal-data-sheet-model.js
@@ -1,11 +1,23 @@
-exports.generateID = () => {
-  const sql = 'INSERT INTO PersonalDataSheet VALUES (NULL)';
+const mysql = require('mysql')
+const connection =  mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+  connection.connect()
+
+exports.generateID = new Promise((resolve) => {
+  const sql = 'INSERT INTO PersonalDataSheet(PersonalDataSheetId) VALUES (NULL)';
+  // const sql = 'SELECT * FROM dhl_db.PersonalDataSheet'
 
   connection.query(sql, (error, results) => {
     if (typeof results !== 'undefined') {
       const personalDataSheetID = results.insertId;
-      return personalDataSheetID;
+      resolve(personalDataSheetID);
     }
     return null;
   });
-};
+}).then(val => {return val})
+
