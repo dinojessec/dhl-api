97e00541eaa9c689e7fa82cb9970ec3c7bd110df
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-12 21:27:03 +0800

    generate and update role on user route

--- models/role-model.js
+++ models/role-model.js
@@ -1,9 +1,37 @@
 const role = {
-    generateRoleID() {
-        const sql = 'SELECT * FROM Role WHERE roleID = 1';
+    generateRoleID(roleID) {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT Student.studentID, Student.firstName, Student.middleName, Student.lastName, Role.roleID, Role.roleName
+                        FROM Role
+                            LEFT JOIN Student
+                                ON Role.roleID = Student.roleID
+                            WHERE Student.roleID = ${roleID}`;
 
-        return sql;
+            connection.query(sql, (err, result) => {
+                if (err) {
+                    console.log('roleID error', err);
+                } else {
+                    resolve(result);
+                }
+            });
+            // resolve(sql);
+        });
     },
+
+    updateRole(params) {
+        return new Promise((resolve, reject) => {
+            const sql = `UPDATE Student SET roleID = ${params.roleID} WHERE studentID = ${params.studentID}`;
+
+            connection.query(sql, (err, result) => {
+                if (err) {
+                    console.log('UPDATE ROLE ERROR', ERR);
+                } else {
+                    resolve(result);
+                }
+            });
+            // resolve(sql);
+        })
+    }
 };
 
 module.exports = role;
