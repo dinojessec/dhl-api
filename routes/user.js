662e400a20b7259743f69b9f766a4a5ff2f3a184
Jesse Dino <jesse.dino.z3@gmail.com>
2019-04-19 18:32:05 +0800

    clean console.logs

--- routes/user.js
+++ routes/user.js
@@ -8,17 +8,17 @@ router.get('/:roleID', (req, res) => {
     const roleID = req.params.roleID;
 
     roleModel.generateRoleID(roleID).then(response => {
-        console.log(response);
+        // console.log(response);
         res.json({ result: response });
     });
 });
 
 router.put('/', (req, res) => {
     const params = req.body;
-    console.log(params);
+    // console.log(params);
 
     roleModel.updateRole(params).then(response => {
-        console.log(response);
+        // console.log(response);
         res.json({ response });
     })
         .catch(e => {
